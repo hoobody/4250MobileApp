df4ad0feb5fa772e5242587795cd7fb599648666
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-01-31 00:54:22 -0800

    Added checks to change the background and border color to red (and back) in MonsterCreatePage.xaml

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -142,7 +142,26 @@ namespace Game.Views
 
         public void Name_onTextChange(object sender, ValueChangedEventArgs e)
         {
-
+            if (string.IsNullOrEmpty(NameEntry.Text))
+            {
+                NameEntry.BackgroundColor = Color.FromHex("848884");
+                NameFrame.BorderColor = Color.Red;
+            }
+            if (string.IsNullOrWhiteSpace(NameEntry.Text))
+            {
+                NameEntry.BackgroundColor = Color.FromHex("848884");
+                NameFrame.BorderColor = Color.Red;
+            }
+            if (!string.IsNullOrEmpty(NameEntry.Text))
+            {
+                NameEntry.BackgroundColor = Color.FromHex("36454F");
+                NameFrame.BorderColor = Color.FromHex("#696969");
+            }
+            if (!string.IsNullOrWhiteSpace(NameEntry.Text))
+            {
+                NameEntry.BackgroundColor = Color.FromHex("36454F");
+                NameFrame.BorderColor = Color.FromHex("#696969");
+            }
         }
 
 
