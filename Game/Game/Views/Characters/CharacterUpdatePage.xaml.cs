ad3e5cb91d2ddee726c495bd4612c19203d0b4aa
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-10 00:33:34 -0800

    Changed style in CharacterUpdatePage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -479,13 +479,13 @@ namespace Game.Views
 
             if (string.IsNullOrEmpty(NameEntry.Text))
             {
-                NameEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                NameEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 NameFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
 
             if (string.IsNullOrWhiteSpace(NameEntry.Text))
             {
-                NameEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                NameEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 NameFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
         }
@@ -503,13 +503,13 @@ namespace Game.Views
 
             if (string.IsNullOrEmpty(DescriptionEntry.Text))
             {
-                DescriptionEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                DescriptionEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 DescriptionFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
 
             if (string.IsNullOrWhiteSpace(DescriptionEntry.Text))
             {
-                DescriptionEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                DescriptionEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 DescriptionFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
         }
@@ -528,13 +528,13 @@ namespace Game.Views
 
             if (string.IsNullOrEmpty(CodenameEntry.Text))
             {
-                CodenameEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                CodenameEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 CodenameFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
 
             if (string.IsNullOrWhiteSpace(CodenameEntry.Text))
             {
-                CodenameEntry.BackgroundColor = (Color)Application.Current.Resources["SecondaryBackgroundColor"];
+                CodenameEntry.BackgroundColor = (Color)Application.Current.Resources["TriciaryBackgroundColor"];
                 CodenameFrame.BorderColor = (Color)Application.Current.Resources["Error"];
             }
         }
