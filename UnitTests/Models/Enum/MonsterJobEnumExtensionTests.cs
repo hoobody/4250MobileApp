bc9303485c524c5db9d480aea9b60ddccbdcd338
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 12:59:26 -0800

    Removed s in MonsterJobEnumsExtensionTest.cs Added test methods to MonsterJobEnumExtensionTest.cs

---
+++ UnitTests/Models/Enum/MonsterJobEnumExtensionTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class MonsterJobEnumExtensionTest
+    {
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterJobEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Player", result);
+        }
+
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Fighter_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterJobEnum.Bodyguard.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Bodyguard", result);
+        }
+
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Cleric_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterJobEnum.Mercenary.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Mercenary", result);
+        }
+    }
+}
