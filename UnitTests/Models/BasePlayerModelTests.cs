9f13af95b9bbfd11e40267df5a91b28cb005bd0c
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 23:26:15 -0800

    Modified BasePlayerModel_BuffSpeedJobBonus_Assassin_Should_Pass method in BasePlayerModelTests.cs

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1179,8 +1179,8 @@ namespace UnitTests.Models
             var data = new BasePlayerModel<CharacterModel>();
             data.Job = CharacterJobEnum.Assassin;
 
-            // Should equal the same since Speed is set to 0
-            Assert.AreEqual(data.GetSpeedJobBonus, data.Speed + data.GetSpeedJobBonus);
+            // Should equal the same since Speed is set to 1
+            Assert.AreEqual(data.GetSpeedJobBonus, data.GetSpeed() - 1);
         }
 
         [Test]
