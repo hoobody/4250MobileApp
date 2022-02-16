465ce3ba2e8bbb15067308b9c48ddc716a752dbb
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 22:49:29 -0800

    Added BasePlayerModel_BuffSpeedJobBonus_SpecialAgent_Should_Pass

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1224,6 +1224,16 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_BuffSpeedJobBonus_SpecialAgent_Should_Pass()
+        {
+            var data = new BasePlayerModel<CharacterModel>();
+            data.Job = CharacterJobEnum.SpecialAgent;
+
+            // Should equal the same since Speed is set to 0
+            Assert.AreEqual(data.GetSpeedJobBonus, data.Speed + data.GetSpeedJobBonus);
+        }
+
+        [Test]
         public void BasePlayerModel_BuffHealth_Default_Should_Pass()
         {
             var data = new BasePlayerModel<CharacterModel>();
