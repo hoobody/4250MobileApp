0d13b491f95ec8a38c5bea797c789bda4fa132f0
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 22:50:43 -0800

    Added BasePlayerModel_BuffSpeedJobBonus_Fighter_Should_Pass to BasePlayerModelTest.cs

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1244,6 +1244,16 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_BuffSpeedJobBonus_Fighter_Should_Pass()
+        {
+            var data = new BasePlayerModel<CharacterModel>();
+            data.Job = CharacterJobEnum.Fighter;
+
+            // Should equal the same since Speed is set to 0
+            Assert.AreEqual(data.GetSpeedJobBonus, data.Speed + data.GetSpeedJobBonus);
+        }
+
+        [Test]
         public void BasePlayerModel_BuffHealth_Default_Should_Pass()
         {
             var data = new BasePlayerModel<CharacterModel>();
