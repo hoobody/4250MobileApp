96de34c198e08b24f613bfd6dec01207ef0c5451
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 22:43:39 -0800

    Added BasePlayerModel_BuffSpeedJobBonus_Assassin_Should_Pass method to BasePlayerModelTest.cs

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1174,6 +1174,16 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_BuffSpeedJobBonus_Assassin_Should_Pass()
+        {
+            var data = new BasePlayerModel<CharacterModel>();
+            data.Job = CharacterJobEnum.Assassin;
+
+            // Should equal the same since Speed is set to 0
+            Assert.AreEqual(data.GetSpeedJobBonus, data.Speed + data.GetSpeedJobBonus);
+        }
+
+        [Test]
         public void BasePlayerModel_BuffHealth_Default_Should_Pass()
         {
             var data = new BasePlayerModel<CharacterModel>();
