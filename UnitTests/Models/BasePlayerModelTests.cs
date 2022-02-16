3ec011cf67ab14d341ff44cc122b3cc603a73969
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-15 22:37:15 -0800

    Added BasePlayerModel_BuffSpeedJobBonus_SurveillanceOfficer_Should_Pass method to BasePlayerModelTests.cs

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1164,6 +1164,16 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_BuffSpeedJobBonus_SurveillanceOfficer_Should_Pass()
+        {
+            var data = new BasePlayerModel<CharacterModel>();
+            data.Job = CharacterJobEnum.SurveillanceOfficer;
+
+            // Should equal the same since Speed is set to 0
+            Assert.AreEqual(data.GetSpeedJobBonus, data.Speed + data.GetSpeedJobBonus);
+        }
+
+        [Test]
         public void BasePlayerModel_BuffHealth_Default_Should_Pass()
         {
             var data = new BasePlayerModel<CharacterModel>();
