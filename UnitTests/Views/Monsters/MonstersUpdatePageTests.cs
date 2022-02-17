52977e786a9fa28f905ce9520916502be91e01fb
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 21:27:14 -0800

    Added MonsterUpdatePage_RollDice_Clicked_Default_Should_Pass method to MonsterUpdatePageTests.cs

--- UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
@@ -107,5 +107,20 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void MonsterUpdatePage_RollDice_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data = new MonsterModel();
+
+            // Act
+            page.Randomize_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
