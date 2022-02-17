460ac14c284507c4b69d8815c974b1ebf341dae7
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 18:39:29 -0800

    Added CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Description_With_Whitespace_Should_Pass method to ItemCreatePageTests.cs

--- UnitTests/Views/Items/ItemCreatePageTests.cs
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -218,5 +218,23 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Description_With_Whitespace_Should_Pass()
+        {
+            // Arrange
+            var DescriptionEntry = page.FindByName("DescriptionEntry");
+            ((Entry)DescriptionEntry).Text = " ";
+
+            page.Description_onTextChange(null, null);
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
