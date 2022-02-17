ce01bac985be039df26ad56a4f4f8c2d0ab02d19
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 22:11:09 -0800

    Added MonsterUpdatePage_CheckifReadyToSubmit_Valid_All_But_Name_With_Whitespace_Should_Pass method in MonsterUpdatePageTests.cs

--- UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
@@ -201,5 +201,23 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void MonsterUpdatePage_CheckifReadyToSubmit_Valid_All_But_Name_With_Whitespace_Should_Pass()
+        {
+            // Arrange
+            var NameEntry = page.FindByName("NameEntry");
+            ((Entry)NameEntry).Text = " ";
+
+            page.Name_onTextChange(null, null);
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
