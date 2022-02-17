ab9a7934647a2b2719bbd52abdca6251c9c6ffa8
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 18:38:18 -0800

    Added CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Name_Should_Pass method to ItemCreatePageTests.cs

--- UnitTests/Views/Items/ItemCreatePageTests.cs
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -182,5 +182,23 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Name_Should_Pass()
+        {
+            // Arrange
+            var NameEntry = page.FindByName("NameEntry");
+            ((Entry)NameEntry).Text = "";
+
+            page.Name_onTextChange(null, null);
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
