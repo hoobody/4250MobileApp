2d044df0339d74a9c91680389754e4ecb7d5bab0
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 16:50:34 -0800

    Added CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Codename_Should_Pass method to CharacterCreatePageTests.cs

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -210,6 +210,24 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void CharacterCreatePage_CheckifReadyToSubmit_Valid_All_But_Codename_Should_Pass()
+        {
+            // Arrange
+            var CodenameEntry = page.FindByName("CodenameEntry");
+            ((Entry)CodenameEntry).Text = "";
+
+            page.Codename_onTextChange(null, null);
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
         [Test]
         public void CharacterCreatePage_RollDice_Clicked_Default_Should_Pass()
         {
