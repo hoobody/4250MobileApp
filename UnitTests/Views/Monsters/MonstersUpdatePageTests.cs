22ee4dbe6c9a9decd0d4f838f2f58c9408b1a86e
Jonathan Doan <doanjonathan@seattleu.edu>
2022-02-16 22:07:05 -0800

    Added MonsterUpdatePage_ClosePopup_Clicked_Default_Should_Pass method in MonsterUpdatePageTests.cs

--- UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
@@ -136,5 +136,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void MonsterUpdatePage_ClosePopup_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ClosePopup_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
