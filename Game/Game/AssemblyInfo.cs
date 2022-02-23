2f6d37aafdc0e2a831cf25a5813fac52f187be96
hoobody <jacob@charbo.net>
2022-02-22 16:43:24 -0800

    Updated Sneakers font name in AssemblyInfo.cs

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using Xamarin.Forms;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 [assembly: ExportFont("OCRFont.ttf", Alias = "OCRFont")]
-[assembly: ExportFont("Sneaker.ttf", Alias = "SneakerFont")]
+[assembly: ExportFont("Sneakers.ttf", Alias = "SneakerFont")]
