5a7a2b6fa15dfc454913ce929998cd246358576e
hoobody <jacob@charbo.net>
2022-02-22 16:20:41 -0800

    Added Sneaker.ttf to AssemblyInfo.cs

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using Xamarin.Forms;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 [assembly: ExportFont("OCRFont.ttf", Alias = "OCRFont")]
+[assembly: ExportFont("Sneaker.ttf", Alias = "SneakerFont")]
