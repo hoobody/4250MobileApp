59e2d2602e2ef4e08ee0f5d3ba8a03c83088d490
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-03-01 16:59:52 -0800

    Got AutoBattleEngine.cs running

--- Game/Game/Engine/EngineGame/AutoBattleEngine.cs
+++ Game/Game/Engine/EngineGame/AutoBattleEngine.cs
@@ -1,6 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
 using Game.Engine.EngineBase;
 using Game.Engine.EngineInterfaces;
+using Game.ViewModels;
+using Game.GameRules;
+using Game.Models;
 
 namespace Game.Engine.EngineGame
 {
@@ -44,17 +50,90 @@ namespace Game.Engine.EngineGame
 
         public override bool CreateCharacterParty()
         {
-            return false;
+            // Picks 6 Characters
+
+            // To use your own characters, populate the List before calling RunAutoBattle
+
+            // Will first pull from existing characters
+            foreach (var data in CharacterIndexViewModel.Instance.Dataset)
+            {
+                if (Battle.EngineSettings.CharacterList.Count >= Battle.EngineSettings.MaxNumberPartyCharacters)
+                {
+                    break;
+                }
+
+                // Start off with max health if adding a character in
+                data.CurrentHealth = data.GetMaxHealthTotal;
+                _ = Battle.PopulateCharacterList(data);
+            }
+
+            //If there are not enough will add random ones
+            for (var i = Battle.EngineSettings.CharacterList.Count; i < Battle.EngineSettings.MaxNumberPartyCharacters; i++)
+            {
+                _ = Battle.PopulateCharacterList(RandomPlayerHelper.GetRandomCharacter(1));
+            }
+
+            return true;
         }
 
         public override bool DetectInfinateLoop()
         {
-            return false;
+            if (Battle.EngineSettings.BattleScore.RoundCount > Battle.EngineSettings.MaxRoundCount)
+            {
+                return true;
+            }
+
+            return Battle.EngineSettings.BattleScore.TurnCount > Battle.EngineSettings.MaxTurnCount;
         }
 
-        public override Task<bool> RunAutoBattle()
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public override async Task<bool> RunAutoBattle()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
-            return Task.FromResult(false);
+            RoundEnum RoundCondition;
+
+            Debug.WriteLine("Auto Battle Starting");
+
+            // Auto Battle, does all the steps that a human would do.
+
+            // Prepare for Battle
+
+            _ = CreateCharacterParty();
+
+            // Start Battle in AutoBattle mode
+            _ = Battle.StartBattle(true);
+
+            // Fight Loop. Continue until Game is Over...
+            do
+            {
+                // Check for excessive duration.
+                if (DetectInfinateLoop())
+                {
+                    Debug.WriteLine("Aborting, More than Max Rounds");
+                    _ = Battle.EndBattle();
+                    return false;
+                }
+
+                Debug.WriteLine("Next Turn");
+
+                // Do the turn...
+                // If the round is over start a new one...
+                RoundCondition = Battle.Round.RoundNextTurn();
+
+                if (RoundCondition == RoundEnum.NewRound)
+                {
+                    _ = Battle.Round.NewRound();
+                    Debug.WriteLine("New Round");
+                }
+
+            } while (RoundCondition != RoundEnum.GameOver);
+
+            Debug.WriteLine("Game Over");
+
+            // Wrap up
+            _ = Battle.EndBattle();
+
+            return true;
         }
     }
 }
