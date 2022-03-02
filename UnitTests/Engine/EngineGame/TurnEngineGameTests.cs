3bf62d8e89c46909c33844afedfc65bb4e8bb4e2
Jonathan Doan <doanjonathan@seattleu.edu>
2022-03-01 20:15:48 -0800

    Changed test case method in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -373,7 +373,7 @@ namespace UnitTests.Engine.EngineGame
             // Reset
 
             // Assert
-            Assert.AreEqual(false, result);
+            Assert.AreEqual(true, result);
         }
         #endregion DetermineCriticalMissProblem
 
