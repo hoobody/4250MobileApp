3809ef9beb4c74e3e40419d1eda66a4eb157928a
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-03-01 21:43:39 -0800

    Added Scenario 9 to hackathonScenariosTests.cs

--- UnitTests/Hackathon/HackathonScenariosTests.cs
+++ UnitTests/Hackathon/HackathonScenariosTests.cs
@@ -1,8 +1,18 @@
 ﻿using NUnit.Framework;
 
 using Game.Models;
+using Game;
 using System.Threading.Tasks;
 using Game.ViewModels;
+using UnitTests.TestHelpers;
+using Game.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Mocks;
+
+
+
 
 namespace Scenario
 {
@@ -149,5 +159,101 @@ namespace Scenario
             Assert.AreEqual(1, EngineViewModel.Engine.EngineSettings.BattleScore.RoundCount);
         }
         #endregion Scenario1
+
+
+        #region Scenario9
+        [Test]
+        public void HackathonScenario_Scenario_9_Valid_Default_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      9
+            *      
+            * Description: 
+            *       Just in time delivery for better loot at the end of rounds
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      RoundOverPage.xaml: Added a button to do the delivery
+            *      RoundOverPage.xaml.cs:
+            *       - MomAndPopShopInstantDelivery_Clicked(): Gets up to six extra items for the characters to select from directly from the HTTPS service
+            *      
+            * 
+            * Test Algrorithm:
+            *      Create app
+            *      Create RoundOver page
+            *      set up a mock http connection
+            *      prepare the battle engine
+            *      start the engine in autobattle mode
+            *      made a new character
+            *      Add them to the character list
+            *      count the items in the pool
+            *      click the delivery button
+            *      count the items again
+            *      reset the connection
+            *      clean up the character list
+            *      clear the app
+            * 
+            * Test Conditions:
+            *      Default condition is sufficient
+            * 
+            * Validation:
+            *      Verify the click worked by counting the number of items retrieved
+            *  
+            */
+
+            //Arrange
+
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            App app = new App();
+            Application.Current = app;
+
+            RoundOverPage page = new RoundOverPage();
+
+            _ = TestBaseHelper.SetHttpClientToMock();
+            ResponseMessage.SetHttpStatusCode(ResponseMessage.HttpStatusCodeSuccess);
+            ResponseMessage.SetResponseMessageStringContent(JsonSampleData.StringContentItemPostDefault);
+
+            _ = BattleEngineViewModel.Instance.Engine.Round.ClearLists();
+
+            //Start the Engine in AutoBattle Mode
+            _ = BattleEngineViewModel.Instance.Engine.StartBattle(false);
+
+            var CharacterPlayer = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = -1, // Will go last...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Jesse",
+                                
+                            });
+
+            BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Add(CharacterPlayer);
+
+            var beforeItemCount = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelDropList.Count;
+
+            //Act
+            page.MomAndPopShopInstantDelivery_Clicked(null, null);
+
+            var afterItemCount = BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelDropList.Count;
+
+            //Reset
+            ResponseMessage.SetResponseMessageStringContent(ResponseMessage.NullStringContent);
+            _ = TestBaseHelper.SetHttpClientToReal();
+
+            BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Remove(CharacterPlayer);
+
+            Application.Current = null;
+
+            // Assert
+            Assert.IsTrue(afterItemCount > beforeItemCount); //We managed to get the items
+        }
+        #endregion Scenario1
+
+
     }
 }
