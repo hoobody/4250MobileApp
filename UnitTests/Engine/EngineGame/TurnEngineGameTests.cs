c7c3bd29edf6c889caf712f8e95b8edccef63ffe
Jonathan Doan <doanjonathan@seattleu.edu>
2022-03-03 13:08:38 -0800

    Added Attack test methods in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -93,19 +93,61 @@ namespace UnitTests.Engine.EngineGame
         #endregion ApplyDamage
 
         #region Attack
-        //[Test]
-        //public void RoundEngine_Attack_Valid_Default_Should_Pass()
-        //{
-        //    // Arrange 
+        [Test]
+        public void TurnEngine_Attack_Valid_Empty_Monster_List_Should_Fail()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel(new CharacterModel());
 
-        //    // Act
-        //    var result = Engine.Round.Turn.Attack(new PlayerInfoModel());
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
 
-        //    // Reset
+            // Reset
+            _ = Engine.StartBattle(false);   // Clear the Engine
 
-        //    // Assert
-        //    Assert.AreEqual(false, result);
-        //}
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TurnEngine_Attack_InValid_Empty_Character_List_Should_Fail()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel(new MonsterModel());
+
+            // Cause an error by making the list empty
+            Engine.EngineSettings.CharacterList.Clear();
+            Engine.EngineSettings.CurrentDefender = null;
+            Engine.EngineSettings.CurrentAttacker = null;
+
+            _ = Engine.StartBattle(true);   // Clear the Engine
+
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
+
+            // Reset
+            _ = Engine.StartBattle(false);   // Clear the Engine
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void TurnEngine_Attack_Valid_Correct_List_Should_Pass()
+        {
+            // Arrange
+            var PlayerInfo = new PlayerInfoModel();
+            Engine.EngineSettings.MonsterList.Add(new PlayerInfoModel(new MonsterModel()));
+
+            // Act
+            var result = Engine.Round.Turn.Attack(PlayerInfo);
+
+            // Reset
+            _ = Engine.StartBattle(false);   // Clear the Engine
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
         #endregion Attack
 
         #region AttackChoice
