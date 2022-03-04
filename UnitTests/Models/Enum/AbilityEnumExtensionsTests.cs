803ff61927b89eb5871f010c09712ef0297f4707
McQuillanJes <94885264+McQuillanJes@users.noreply.github.com>
2022-03-03 23:11:58 -0800

    Added message check tests for class abilities in AbilityuEnumExtensionsTests.cs

--- UnitTests/Models/Enum/AbilityEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/AbilityEnumExtensionsTests.cs
@@ -132,5 +132,123 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual("Toughen Up", result);
         }
+
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Assassin_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Assassin.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Large Attack Buff, minor Defense and Speed buff", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Spy_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Spy.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Minor attack and Speed buff", result);
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        ///
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Hacker_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Hacker.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Attack debuff, large defense buff, minor speed buff", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_SpecialAgent_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.SpecialAgent.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Minor buffs to attack, defense, and speed", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Sabateur_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Saboteur.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Attack debuff, minor defense and speed buffs", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_SurvOfficer_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.SurveillanceOfficer.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Major speed buff, minor attack and defense buffs", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Detective_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Detective.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Small attack buff, moderate defense buff, speed debuff", result);
+        }
+
+        [Test]
+        public void AbilityEnumExtensionsTests_Double0_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = AbilityEnum.Double0.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Buffs to attack, debuff to defense, and minor buff to speed", result);
+        }
+
+
     }
 }
