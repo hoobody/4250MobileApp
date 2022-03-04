744dcfee5dd4bc85ae74668a089089740e5fff41
hoobody <jacob@charbo.net>
2022-03-03 16:31:33 -0800

    Added PickupItemsFromPool() from mike engine in RoundEngine.cs

--- Game/Game/Engine/EngineGame/RoundEngine.cs
+++ Game/Game/Engine/EngineGame/RoundEngine.cs
@@ -361,7 +361,19 @@ namespace Game.Engine.EngineGame
 
             // I use the same logic for Auto Battle as I do for Manual Battle
 
-            return base.PickupItemsFromPool(character);
+            //if (BaseEngine.BattleScore.AutoBattle)
+            {
+                // Have the character, walk the items in the pool, and decide if any are better than current one.
+
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.Head);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.Necklass);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.PrimaryHand);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.OffHand);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.RightFinger);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.LeftFinger);
+                _ = GetItemFromPoolIfBetter(character, ItemLocationEnum.Feet);
+            }
+            return true;
         }
 
         /// <summary>
