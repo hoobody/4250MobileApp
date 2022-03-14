a78ec782d584d474e0d2347da287235dd1e3304d
Jonathan Doan <doanjonathan@seattleu.edu>
2022-03-13 21:16:17 -0700

    Added TurnEngine_UseAbility_Valid_Ability_Assassin_Should_Pass() test method in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -622,6 +622,21 @@ namespace UnitTests.Engine.EngineGame
             // Assert
             Assert.AreEqual(true, result);
         }
+
+        [Test]
+        public void TurnEngine_UseAbility_Valid_Ability_Assassin_Should_Pass()
+        {
+            // Arrange
+
+            var characterPlayer = new PlayerInfoModel(new CharacterModel { Job = CharacterJobEnum.Assassin });
+
+            // Act
+            var result = BattleEngineViewModel.Instance.Engine.Round.Turn.UseAbility(characterPlayer);
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
         #endregion UseAbility
 
         #region DropItems
