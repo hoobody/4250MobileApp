a84c8f9f6385a93aa469af9d5015611cb6ddbf28
Jonathan Doan <doanjonathan@seattleu.edu>
2022-03-13 21:18:56 -0700

    Added TurnEngine_UseAbility_Valid_Ability_Sabotuer_Should_Pass() test method in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -682,6 +682,21 @@ namespace UnitTests.Engine.EngineGame
             // Assert
             Assert.AreEqual(true, result);
         }
+
+        [Test]
+        public void TurnEngine_UseAbility_Valid_Ability_Sabotuer_Should_Pass()
+        {
+            // Arrange
+
+            var characterPlayer = new PlayerInfoModel(new CharacterModel { Job = CharacterJobEnum.Saboteur });
+
+            // Act
+            var result = BattleEngineViewModel.Instance.Engine.Round.Turn.UseAbility(characterPlayer);
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
         #endregion UseAbility
 
         #region DropItems
