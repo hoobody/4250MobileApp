6f8cca583fee9800517ea817a6402825d7f48700
Jonathan Doan <doanjonathan@seattleu.edu>
2022-03-13 21:20:51 -0700

    Added TurnEngine_UseAbility_Valid_Ability_Spy_Should_Pass test method in TurnEngineGameTests.cs

--- UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/TurnEngineGameTests.cs
@@ -727,6 +727,21 @@ namespace UnitTests.Engine.EngineGame
             // Assert
             Assert.AreEqual(true, result);
         }
+
+        [Test]
+        public void TurnEngine_UseAbility_Valid_Ability_Spy_Should_Pass()
+        {
+            // Arrange
+
+            var characterPlayer = new PlayerInfoModel(new CharacterModel { Job = CharacterJobEnum.Spy });
+
+            // Act
+            var result = BattleEngineViewModel.Instance.Engine.Round.Turn.UseAbility(characterPlayer);
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
         #endregion UseAbility
 
         #region DropItems
